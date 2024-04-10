d4ee4475a0daef0375c37e48f9f4597abed7a85c
Quang Le <levuquang2k1@gmail.com>
2024-04-10 23:17:28 +0700

    auto migration when start api server

--- cmd/app/api/api.go
+++ cmd/app/api/api.go
@@ -9,6 +9,7 @@ import (
 	"github.com/vuquang23/poseidon/internal/pkg/config"
 	"github.com/vuquang23/poseidon/internal/pkg/server"
 	"github.com/vuquang23/poseidon/pkg/logger"
+	"github.com/vuquang23/poseidon/pkg/postgres"
 )
 
 func RunAPI(c *cli.Context) error {
@@ -20,6 +21,17 @@ func RunAPI(c *cli.Context) error {
 	// logger
 	_, err := logger.Init(conf.Log, logger.LoggerBackendZap)
 	if err != nil {
+		return err
+	}
+
+	// postgres
+	db, err := postgres.New(conf.Postgres)
+	if err != nil {
+		return err
+	}
+
+	// auto migration
+	if err := postgres.MigrateUp(db, "file://./migration/postgres", 0); err != nil {
 		return err
 	}
 
